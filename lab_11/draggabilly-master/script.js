b533eb5e0d8cd985c211e787a792ae4cb34c41d0
clintlinn12 <clintlin@usc.edu>
2023-11-17 20:25:52 -0800

    lab 11 submission

--- lab_11/draggabilly-master/script.js
+++
@@ -1,7 +0,0 @@
-var draggableElems = document.querySelectorAll('.draggable');
-var draggies = []
-for ( var i=0; i < draggableElems.length; i++ ) {
-  var draggableElem = draggableElems[i];
-  var draggie = new Draggabilly( draggableElem, {});
-  draggies.push( draggie );
-}
